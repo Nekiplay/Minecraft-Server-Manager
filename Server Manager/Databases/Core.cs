ab411c33d27ef27ca1a6d1251824b03f1e962788
Nekiplay <Nekiplay@DESKTOP-RKN5HA1>
2021-11-26 21:03:30 +0500

    KCauldron Support

--- Server Manager/Databases/Core.cs
+++ Server Manager/Databases/Core.cs
@@ -12,6 +12,12 @@ namespace Server_Manager.Databases
 
             /* PaperSpigot */
 
+            /* KCauldron */
+            new CoreInformation("KCauldron", "1420.120", "1.7.10", "D5F676A6200ADC293F3D1FDBE6218F25"),
+            new CoreInformation("KCauldron", "1448.133", "1.12.2", "23ADDC86C9D58E466A19BFDFFA351466"),
+            new CoreInformation("KCauldron", "1481.148", "1.12.2", "CC6080255E72BDACD1DDEE6491FEDFB2"),
+            new CoreInformation("KCauldron", "1492.152", "1.12.2", "009DCB31F9E002CAFD212A3DA9DD8F0C"),
+
             /* Forge */
             new CoreInformation("Forge", "10.13.4.1614", "1.7.10", "ADD0FBA161C4652A96EFB4264EC2D9EC"),
             new CoreInformation("Forge", "14.23.5.2855", "1.12.2", "02FCC447BB8938E5214292E4D36EC949"),
