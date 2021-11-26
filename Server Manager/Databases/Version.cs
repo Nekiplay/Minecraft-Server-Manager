4b78486082458fee5e69cc59780f39c785535ede
Nekiplay <Nekiplay@DESKTOP-RKN5HA1>
2021-11-26 23:32:30 +0500

    Versions list+

--- Server Manager/Databases/Version.cs
+++ Server Manager/Databases/Version.cs
@@ -12,6 +12,44 @@
                 return "1.8.9";
             case (Versions.Version_1_12_2):
                 return "1.12.2";
+            case (Versions.Version_1_13):
+                return "1.13";
+            case (Versions.Version_1_13_1):
+                return "1.13.1";
+            case (Versions.Version_1_13_2):
+                return "1.13.2";
+            case (Versions.Version_1_14):
+                return "1.14";
+            case (Versions.Version_1_14_1):
+                return "1.14.1";
+            case (Versions.Version_1_14_2):
+                return "1.14.2";
+            case (Versions.Version_1_14_3):
+                return "1.14.3";
+            case (Versions.Version_1_14_4):
+                return "1.14.4";
+            case (Versions.Version_1_15):
+                return "1.15";
+            case (Versions.Version_1_15_1):
+                return "1.15.1";
+            case (Versions.Version_1_15_2):
+                return "1.15.2";
+            case (Versions.Version_1_16):
+                return "1.16";
+            case (Versions.Version_1_16_1):
+                return "1.16.1";
+            case (Versions.Version_1_16_2):
+                return "1.16.2";
+            case (Versions.Version_1_16_3):
+                return "1.16.3";
+            case (Versions.Version_1_16_4):
+                return "1.16.4";
+            case (Versions.Version_1_16_5):
+                return "1.16.5";
+            case (Versions.Version_1_17):
+                return "1.17";
+            case (Versions.Version_1_17_1):
+                return "1.17.1";
             default:
                 return "Unknow";
         }
@@ -24,4 +62,23 @@ public enum Versions
     Version_1_7_10,
     Version_1_8_9,
     Version_1_12_2,
+    Version_1_13,
+    Version_1_13_1,
+    Version_1_13_2,
+    Version_1_14,
+    Version_1_14_1,
+    Version_1_14_2,
+    Version_1_14_3,
+    Version_1_14_4,
+    Version_1_15,
+    Version_1_15_1,
+    Version_1_15_2,
+    Version_1_16,
+    Version_1_16_1,
+    Version_1_16_2,
+    Version_1_16_3,
+    Version_1_16_4,
+    Version_1_16_5,
+    Version_1_17,
+    Version_1_17_1,
 }
