01a75192e8b6dfa050b0037d1558cc3c0314845f
Nekiplay <Nekiplay@DESKTOP-RKN5HA1>
2021-11-27 00:01:23 +0500

    Version list +

--- Server Manager/Databases/Version.cs
+++ Server Manager/Databases/Version.cs
@@ -6,10 +6,64 @@
         {
             case (Versions.Version_null):
                 return "Unknow";
+            case (Versions.Version_1_4_6):
+                return "1.4.6";
+            case (Versions.Version_1_4_7):
+                return "1.4.7";
+            case (Versions.Version_1_5_1):
+                return "1.5.2";
+            case (Versions.Version_1_5_2):
+                return "1.5.2";
+            case (Versions.Version_1_6_2):
+                return "1.6.2";
+            case (Versions.Version_1_6_4):
+                return "1.6.4";
+            case (Versions.Version_1_7_2):
+                return "1.7.2";
+            case (Versions.Version_1_7_5):
+                return "1.7.5";
+            case (Versions.Version_1_7_8):
+                return "1.7.8";
+            case (Versions.Version_1_7_9):
+                return "1.7.9";
             case (Versions.Version_1_7_10):
                 return "1.7.10";
+            case (Versions.Version_1_8):
+                return "1.8";
+            case (Versions.Version_1_8_2):
+                return "1.8.2";
+            case (Versions.Version_1_8_3):
+                return "1.8.3";
+            case (Versions.Version_1_8_4):
+                return "1.8.4";
+            case (Versions.Version_1_8_5):
+                return "1.8.5";
+            case (Versions.Version_1_8_6):
+                return "1.8.6";
+            case (Versions.Version_1_8_7):
+                return "1.8.7";
+            case (Versions.Version_1_8_8):
+                return "1.8.8";
             case (Versions.Version_1_8_9):
                 return "1.8.9";
+            case (Versions.Version_1_9):
+                return "1.9";
+            case (Versions.Version_1_9_2):
+                return "1.9.2";
+            case (Versions.Version_1_9_4):
+                return "1.9.4";
+            case (Versions.Version_1_10):
+                return "1.10";
+            case (Versions.Version_1_10_1):
+                return "1.10.1";
+            case (Versions.Version_1_10_2):
+                return "1.10.2";
+            case (Versions.Version_1_11):
+                return "1.11";
+            case (Versions.Version_1_11_1):
+                return "1.11.1";
+            case (Versions.Version_1_11_2):
+                return "1.11.2";
             case (Versions.Version_1_12_2):
                 return "1.12.2";
             case (Versions.Version_1_13):
@@ -59,8 +113,37 @@
 public enum Versions
 {
     Version_null,
+    Version_1_4_6,
+    Version_1_4_7,
+    Version_1_5_1,
+    Version_1_5_2,
+    Version_1_6_2,
+    Version_1_6_4,
+    Version_1_7_2,
+    Version_1_7_5,
+    Version_1_7_8,
+    Version_1_7_9,
     Version_1_7_10,
+    Version_1_8,
+    Version_1_8_2,
+    Version_1_8_3,
+    Version_1_8_4,
+    Version_1_8_5,
+    Version_1_8_6,
+    Version_1_8_7,
+    Version_1_8_8,
     Version_1_8_9,
+    Version_1_9,
+    Version_1_9_2,
+    Version_1_9_4,
+    Version_1_10,
+    Version_1_10_1,
+    Version_1_10_2,
+    Version_1_11,
+    Version_1_11_1,
+    Version_1_11_2,
+    Version_1_12,
+    Version_1_12_1,
     Version_1_12_2,
     Version_1_13,
     Version_1_13_1,
